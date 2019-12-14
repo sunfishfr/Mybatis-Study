ceea92b5c6ac0a452119d398058a6bcb06ef2b03
luhan <719358293@qq.com>
2019-12-15 01:54:33 +0800

    1.User的CRUD；

--- mybatis-01/src/main/java/com/kuang/mapper/UserMapper.java
+++ mybatis-01/src/main/java/com/kuang/mapper/UserMapper.java
@@ -5,5 +5,18 @@ import com.kuang.pojo.User;
 import java.util.List;
 
 public interface UserMapper {
+     //查询所有用户
      List<User> getUserList();
+
+     //根据ID查询
+     User getUserById(int id);
+
+     //insert用户
+     int addUser(User user);
+
+     //修改用户
+     int updateUser(User user);
+
+     //删除用户
+     int deleteUserById(int id);
 }
