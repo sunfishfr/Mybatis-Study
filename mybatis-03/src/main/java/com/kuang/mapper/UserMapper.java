73eb5ce76386d6c57c32591e6e56340efa0d4fd3
luhan <719358293@qq.com>
2019-12-16 00:22:53 +0800

    1.ResultMap，结果集映射，当表中字段与实体类属性不一致时；

---
+++ mybatis-03/src/main/java/com/kuang/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.kuang.mapper;
+
+import com.kuang.pojo.User;
+
+import java.util.List;
+import java.util.Map;
+
+public interface UserMapper {
+
+
+    //根据ID查询
+    User getUserById(int id);
+
+
+}
---
+++ mybatis-03/src/test/java/com/kuang/mapper/UserMapperTest.java
@@ -0,0 +1,27 @@
+package com.kuang.mapper;
+
+import com.kuang.pojo.User;
+import com.kuang.utils.MybatisUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.List;
+
+public class UserMapperTest {
+
+    @Test
+    public void getUserById() {
+        //1.获得SqlSession对象
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+
+        User user = userMapper.getUserById(1);
+
+        System.out.println(user);
+        //4.关闭
+        sqlSession.close();
+    }
+
+
+}
